a547a41950bd40dcde7a6036e648d74d5d6b60b5
Victor Viriya-ampanond <wviriya@microsoft.com>
2020-05-22 13:39:16 +1200

    added pod id and secrets store driver

---
+++ scripts/cluster_config.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+SUBSCRIPTION_ID=$subscriptionId
+RESOURCE_GROUP=$resourceGroup
+CLUSTER_NAME="$clusterName"
+NAME_SPACES=("test" "uat" "prod")
+IDENTITY_NAME="${clusterName}-podid"
+
+az aks get-credentials -n $CLUSTER_NAME -g $RESOURCE_GROUP
+
+for namespace in ${NAME_SPACES[@]}; do
+     # create namespace
+     kubectl create namespace $namespace
+done
+
+# Deploy aad-pod-identity
+kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master/deploy/infra/mic-exception.yaml
+
+az identity create -g $RESOURCE_GROUP -n $IDENTITY_NAME --subscription $SUBSCRIPTION_ID
+
+IDENTITY_CLIENT_ID="$(az identity show -g $RESOURCE_GROUP -n $IDENTITY_NAME --subscription $SUBSCRIPTION_ID --query clientId -otsv)"
+IDENTITY_RESOURCE_ID="$(az identity show -g $RESOURCE_GROUP -n $IDENTITY_NAME --subscription $SUBSCRIPTION_ID --query id -otsv)"
+IDENTITY_ASSIGNMENT_ID="$(az role assignment create --role Reader --assignee $IDENTITY_CLIENT_ID --scope /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP --query id -otsv)"
+
+cat <<EOF | kubectl apply -f -
+apiVersion: "aadpodidentity.k8s.io/v1"
+kind: AzureIdentity
+metadata:
+  name: $IDENTITY_NAME
+spec:
+  type: 0
+  resourceID: $IDENTITY_RESOURCE_ID
+  clientID: $IDENTITY_CLIENT_ID
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: "aadpodidentity.k8s.io/v1"
+kind: AzureIdentityBinding
+metadata:
+  name: $IDENTITY_NAME-binding
+spec:
+  azureIdentity: $IDENTITY_NAME
+  selector: $IDENTITY_NAME
+EOF
+
+
+
