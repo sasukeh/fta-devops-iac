141b651f80ebae02dcaa02083d01269227405cb4
Victor Viriya-ampanond <wviriya@microsoft.com>
2020-05-25 16:34:19 +1200

    use latest TF Azure Provider

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "~> 2.x"
+  version = "~>2.0.0"
   features {}
 }
 
