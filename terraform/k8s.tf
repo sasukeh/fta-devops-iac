0bc2e6735e66e92e465973283ee5cdb1943ec4d2
Victor Viriya-ampanond <wviriya@microsoft.com>
2020-05-25 13:04:33 +1200

    deploy with TF

--- terraform/k8s.tf
+++ terraform/k8s.tf
@@ -140,7 +140,7 @@ resource "random_id" "log_analytics_workspace_name_suffix" {
 
 resource "azurerm_log_analytics_workspace" "test" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}vwv"
+    name                = "${var.cluster_name}-${var.log_analytics_workspace_name}"
     location            = var.log_analytics_workspace_location
     resource_group_name = azurerm_resource_group.k8s.name
     sku                 = var.log_analytics_workspace_sku
